8a9224430b7331c7f7a81cf0f2c28d23d3e49ce5
Gabriel <gaboleal123@gmail.com>
2019-10-30 11:49:43 -0400

    creación de ventana, filtro e importaciones

--- src/org/base/Reproduccion.java
+++ src/org/base/Reproduccion.java
@@ -5,6 +5,23 @@
  */
 package org.base;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.swing.JFileChooser;
+import org.reproductor.Ventana;
+import org.util.Filtro;
+import java.awt.Toolkit;
+import javax.swing.Icon;
+import javax.swing.JDialog;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.filechooser.FileSystemView;
+import javax.swing.filechooser.FileView;
+import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
+
 /**
  *
  * @author Gabriel
