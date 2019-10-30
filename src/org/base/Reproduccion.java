10b01be5b4b5ecd91ba082769d36bdc7fa413397
Gabriel <gaboleal123@gmail.com>
2019-10-30 12:11:28 -0400

    Definicion de la clase filtro, construccion de clases lista reproduccion y ventana, creacion de metodos en reproduccion

--- src/org/base/Reproduccion.java
+++ src/org/base/Reproduccion.java
@@ -28,4 +28,94 @@ import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
  */
 public class Reproduccion {
     
+    private Ventana ventana;
+    private EmbeddedMediaPlayerComponent mediaPlayer;
+    private String ficheroVideo;
+	
+    public ArrayList<String> lista;
+    private boolean fin = false;
+    private boolean acabar = false;
+    
+    public void inicializar(Ventana ventana){
+		this.ventana = ventana;
+		this.mediaPlayer = ventana.mediaPlayer;
+	}
+    
+    public void abrirArchivo(){
+            JFileChooser fileChooser = new JFileChooser(new File("Videos"));
+            fileChooser.setDialogTitle("Abrir Archivo de Audio o Video");
+            fileChooser.setFileView(new FileView(){
+                @Override
+                public Icon getIcon(File f){
+                    return FileSystemView.getFileSystemView().getSystemIcon(f);
+                }
+            });
+		fileChooser.setFileFilter(new Filtro());
+                fileChooser.setAcceptAllFileFilterUsed(false);
+                JDialog dialogo = new JDialog();
+                dialogo.setIconImage(Toolkit.getDefaultToolkit().getImage("icon.png"));
+		if (fileChooser.showOpenDialog(dialogo) == JFileChooser.CANCEL_OPTION)
+			return;
+		
+		ficheroVideo = fileChooser.getSelectedFile().getAbsolutePath();
+		
+		habilitar();
+		reproducir(ficheroVideo);
+	}
+    
+    public void habilitar(){
+		
+		ventana.btAdelantar.setEnabled(true);
+		ventana.btPause.setEnabled(true);
+		ventana.btPlay.setEnabled(false);
+		ventana.btRetrasar.setEnabled(true);
+		ventana.btStop.setEnabled(true);
+		
+		ventana.slTiempo.setEnabled(true);
+		ventana.slVolumen.setEnabled(true);
+		
+		
+	}
+    public void reproducir(String fichero){
+		
+		try {
+                    try (PrintWriter reciente = new PrintWriter (new BufferedWriter(new FileWriter("Reciente", true)))) {
+                        reciente.println(fichero);
+                    }
+		} catch (IOException e1) {}
+		
+		ventana.frame.setTitle("Zafiro Players - "+new File(fichero).getName());
+		mediaPlayer.getMediaPlayer().playMedia(fichero);
+		
+		ventana.dimension = mediaPlayer.getMediaPlayer().getCropGeometry();
+		
+		Thread hilo = new Thread(new Runnable(){
+			
+			@Override
+			public void run() {
+				while(mediaPlayer.getMediaPlayer().getLength() == 0){
+					try {
+						Thread.sleep(500);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				ventana.slTiempo.setMaximum((int) mediaPlayer.getMediaPlayer().getLength());
+				
+				while(mediaPlayer.getMediaPlayer().getTime() < mediaPlayer.getMediaPlayer().getLength()){
+					if(ventana.tiempo == true){
+						try {
+							Thread.sleep(500);
+							
+							ventana.slTiempo.setValue((int) mediaPlayer.getMediaPlayer().getTime());
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+				fin = true;
+			}
+		});
+		hilo.start();
+	}
 }
--- src/org/reproductor/Ventana.java
+++ src/org/reproductor/Ventana.java
@@ -5,10 +5,90 @@
  */
 package org.reproductor;
 
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import uk.co.caprica.vlcj.binding.LibVlc;
+import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
+import uk.co.caprica.vlcj.player.embedded.DefaultFullScreenStrategy;
+import uk.co.caprica.vlcj.player.embedded.FullScreenStrategy;
+import uk.co.caprica.vlcj.runtime.RuntimeUtil;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionEvent;
+import javax.swing.JSlider;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import com.sun.jna.Native;
+import com.sun.jna.NativeLibrary;
+import javax.swing.event.ChangeEvent;
+import javax.swing.JLabel;
+import org.base.Reproduccion;
+import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.*;
+import java.awt.Toolkit;
+
+
 /**
  *
  * @author Gabriel
  */
-public class Ventana {
+public class Ventana implements FullScreenStrategy{
+    
+        public JFrame frame;
+	private JMenuItem mntmAbrir;
+	private JMenuItem mntmSalir;
+	private JMenuItem mntmAbrirCds;
+	private JMenuItem mntmTamano;
+	private JMenuItem mntmSubtitulos;
+	private JMenuItem mntmListaDeReproduccion;
+	private JMenuItem mntmRecientes;
+	private JMenuItem mntmCaptura;
+	private JMenuItem menuItem;
+	private JMenuItem menuItem_1;
+	private JMenuItem mntmPantallaCompleta;
+	private JMenuItem mntmPredeterminada;
+	
+	public JButton btRetrasar;
+	public JButton btPlay;
+	public JButton btPause;
+	public JButton btStop;
+	public JButton btAdelantar;
+	public JSlider slVolumen;
+	public JSlider slTiempo;
+        
+	private static String ruta_libreria; //= "C:\\Program Files (x86)\\VideoLAN\\VLC";
+	
+	public EmbeddedMediaPlayerComponent mediaPlayer;
+	private JLabel lbVolumen;
+	public boolean tiempo = true;
+	public String dimension;
+	public Reproduccion reproduccion;
+	private ListaReproduccion listaReproduccion;
+	private Recientes reciente;
+	private JPanel panel;
+	private JMenuItem mntmAbrirStriming;
+	private JMenuItem mntmRetornarPantalla;
+	private FullScreenStrategy fullScreenStrategy;
+    
+
+    @Override
+    public void enterFullScreenMode() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void exitFullScreenMode() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isFullScreenMode() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
